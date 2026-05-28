efa011c3cfd83edea831f772dd491f30dd41b110
Arkadiusz Wojdyla <arkadiusz.wojdyla05@gmail.com>
2026-05-28 22:46:01 +0200

    separate file from audio

--- Compression_cli/src/main.rs
+++ Compression_cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand, ValueEnum};
 use std::fs::File;
 use std::fs::{metadata, Metadata};
 use std::io::Write;
@@ -14,18 +14,40 @@ pub enum CompressionAlgo {
 }
 
 #[derive(Debug, Parser)]
+#[command(name = "Compression CLI")]
 pub struct CliArgs {
-    #[arg(short, long)]
-    pub input: PathBuf,
+    #[command(subcommand)]
+    pub command: Commands,
+}
 
-    #[arg(short, long)]
-    pub output: PathBuf,
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    File {
+        #[arg(short, long)]
+        input: PathBuf,
 
-    #[arg(short, long)]
-    pub compression_algo: CompressionAlgo,
+        #[arg(short, long)]
+        output: PathBuf,
 
-    #[arg(short, long)]
-    pub decompress: bool,
+        #[arg(short, long)]
+        compression_algo: CompressionAlgo,
+
+        #[arg(short, long)]
+        decompress: bool,
+    },
+    Audio {
+        #[arg(short, long)]
+        input: PathBuf,
+
+        #[arg(short, long)]
+        output: PathBuf,
+
+        #[arg(short, long)]
+        compression_algo: CompressionAlgo,
+
+        #[arg(short, long)]
+        decompress: bool,
+    },
 }
 
 fn get_file_size(file_path: &std::path::Path) -> Result<u64, std::io::Error> {
@@ -36,42 +58,57 @@ fn get_file_size(file_path: &std::path::Path) -> Result<u64, std::io::Error> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = CliArgs::parse();
 
-    let mut input_file = File::open(&args.input)?;
-    let mut output_file = File::create(&args.output)?;
+    match args.command {
+        Commands::File {
+            input,
+            output,
+            compression_algo,
+            decompress,
+        } => {
+            let mut input_file = File::open(&input)?;
+            let mut output_file = File::create(&output)?;
 
-    // todo!("validacja argumentów");
+            // todo!("validacja argumentów");
 
-    match args.compression_algo {
-        CompressionAlgo::Rle => {
-            let algo = RleCompression;
-            if args.decompress {
-                algo.decompress(&mut input_file, &mut output_file)?;
-            } else {
-                algo.compress(&mut input_file, &mut output_file)?;
+            match compression_algo {
+                CompressionAlgo::Rle => {
+                    let algo = RleCompression;
+                    if decompress {
+                        algo.decompress(&mut input_file, &mut output_file)?;
+                    } else {
+                        algo.compress(&mut input_file, &mut output_file)?;
+                    }
+                }
             }
+
+            output_file.flush()?;
+
+            let len_input: u64 = get_file_size(&input)?;
+            let len_output: u64 = get_file_size(&output)?;
+
+            println!("");
+            println!("------------------------------------------------------");
+
+            println!(
+                "Udało się skompresować plik przy użyciu algorytmu {:?}",
+                compression_algo
+            );
+            println!("------------------------------------------------------");
+            println!("Początkowy rozmiar pliku: {} b", len_input);
+            println!("Końcowy rozmiar pliku: {} b", len_output);
+            println!(
+                "Różnica rozmiarów: {}%",
+                (len_input as f64 / len_output as f64) * 100.0
+            );
+            println!("------------------------------------------------------");
         }
+        Commands::Audio {
+            input,
+            output,
+            compression_algo,
+            decompress,
+        } => {}
     }
-
-    output_file.flush()?;
-
-    let len_input: u64 = get_file_size(&args.input)?;
-    let len_output: u64 = get_file_size(&args.output)?;
-
-    println!("");
-    println!("------------------------------------------------------");
-
-    println!(
-        "Udało się skompresować plik przy użyciu algorytmu {:?}",
-        args.compression_algo
-    );
-    println!("------------------------------------------------------");
-    println!("Początkowy rozmiar pliku: {} b", len_input);
-    println!("Końcowy rozmiar pliku: {} b", len_output);
-    println!(
-        "Różnica rozmiarów: {}%",
-        (len_input as f64 / len_output as f64) * 100.0
-    );
-    println!("------------------------------------------------------");
 
     Ok(())
 }
